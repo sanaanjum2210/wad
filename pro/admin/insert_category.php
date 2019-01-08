9fcf666f3ed92e5a7be871384cc7bca323c17f39
Sana Anjum <sanaaaanjum22@gmail.com>
2019-01-08 22:33:18 +0500

    Quiz 5 completed

---
+++ pro/admin/insert_category.php
@@ -0,0 +1,59 @@
+<?php
+require_once "db_connection.php";
+print_r($_POST);
+if(isset($_POST['insert_cat'])){
+    print_r($_POST);
+    $category_title = $_POST['cat_title'];
+
+    $insert_cat = "insert into categories (cat_title) 
+                  VALUES ('$category_title');";
+    $insert_cat_result = mysqli_query($con, $insert_cat);
+
+    if($insert_cat){
+        header("location: ".$_SERVER['PHP_SELF']);
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Insert Category</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
+    <style>
+        * {
+            font-family: 'Old Standard TT', serif;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1 class="text-center my-4"><i class="fas fa-plus fa-md"></i> <span class="d-none d-sm-inline"> Add New </span> Product </h1>
+    <form action="insert_category.php" method="post" enctype="multipart/form-data">
+        <div class="row">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_title" class="float-md-right"> <span class="d-sm-none d-md-inline"> Product </span> Title:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-file-signature"></i></div>
+                    </div>
+                    <input type="text" class="form-control" id="cat_title" name="cat_title" placeholder="Enter Product Title" >
+                </div>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto"></div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <button type="submit" name="insert_cat" class="btn btn-primary btn-block"><i class="fas fa-plus"></i> Insert Now </button>
+            </div>
+        </div>
+    </form>
+</div>
+</body>
+</html>
+
